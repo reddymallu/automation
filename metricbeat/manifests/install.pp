d08e189ad3826604d1454f78c1e4771a9a440b1f
Hari Prasad Mallu <hari.mallu@itility.nl>
2019-10-17 13:09:21 +0200

    Update install.pp

--- metricbeat/manifests/install.pp
+++ metricbeat/manifests/install.pp
@@ -5,11 +5,4 @@
 # @example
 #   include metricbeat::install
 class metricbeat::install {
-# Add repo to server
-    yumrepo { 'metricbeatrepo' :
-    enabled  => 1,
-    descr    => 'Metric Beat Repo',
-    baseurl  => 'https://artifacts.elastic.co/downloads/beats/metricbeat/metricbeat-7.4.0-linux-x86_64.tar.gz',
-    gpgcheck => 0,
-    }
 }
